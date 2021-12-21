510282e70895c296eb574aac35978216d55b110c
ennisthemennis <mennis@mit.edu>
2021-12-21 13:13:31 -0500

    add copy for box transfer

--- individual_modules/run_transcription_pull.sh
+++ individual_modules/run_transcription_pull.sh
@@ -28,98 +28,118 @@ fi
 # now start going through patients for the download - do open and psychs separately
 cd "$data_root"/PROTECTED/"$study"/processed
 for p in *; do # loop over all patients in the specified study folder on PHOENIX
-	cd "$p"/interviews/open
-	# create transcripts folder if it hasn't been done for this patient yet
-	if [[ ! -d transcripts ]]; then
-		mkdir transcripts
+	if [[ ! -d ${p}/interviews ]]; then
+		continue
 	fi
-	# also create a folder for the completed audio if needed
-	if [[ ! -d completed_audio ]]; then
-		mkdir completed_audio
+	cd "$p"/interviews
+
+	if [[ -d open ]]; then
+		# create transcripts folder if it hasn't been done for this patient yet
+		if [[ ! -d transcripts ]]; then
+			mkdir transcripts
+		fi
+		# also create a folder for the completed audio if needed
+		if [[ ! -d completed_audio ]]; then
+			mkdir completed_audio
+		fi
+		cd ..
+	fi
+	if [[ -d psychs ]]; then
+		# do the same for psychs
+		cd psychs
+		# create transcripts folder if it hasn't been done for this patient yet
+		if [[ ! -d transcripts ]]; then
+			mkdir transcripts
+		fi
+		# also create a folder for the completed audio if needed
+		if [[ ! -d completed_audio ]]; then
+			mkdir completed_audio
+		fi
+		cd ..
 	fi
 
-	# do the same for psychs
-	cd ../psychs
-	# create transcripts folder if it hasn't been done for this patient yet
-	if [[ ! -d transcripts ]]; then
-		mkdir transcripts
-	fi
-	# also create a folder for the completed audio if needed
-	if [[ ! -d completed_audio ]]; then
-		mkdir completed_audio
-	fi
+	if [[ -d open ]]; then
+		cd open
 
-	cd ../open
+		# this script will go through the pending_audio folder for this patient, check for corresponding named outputs on the transcribeme server, pulling them if available
+		# it will also do file management on the server, update the pending_audio folder accordingly
+		# behaves slightly differently whether this is called individually or via pipeline, because when called via pipeline have email alert related work to do
+		python "$func_root"/interview_transcribeme_sftp_pull.py "open" "$data_root" "$study" "$p" "$transcribeme_username" "$transcribeme_password" "$pipeline" "$repo_root"/transcript_lab_email_body.txt
 
-	# this script will go through the pending_audio folder for this patient, check for corresponding named outputs on the transcribeme server, pulling them if available
-	# it will also do file management on the server, update the pending_audio folder accordingly
-	# behaves slightly differently whether this is called individually or via pipeline, because when called via pipeline have email alert related work to do
-	python "$func_root"/interview_transcribeme_sftp_pull.py "open" "$data_root" "$study" "$p" "$transcribeme_username" "$transcribeme_password" "$pipeline" "$repo_root"/transcript_lab_email_body.txt
+		# now add new info about this patient to email alert body (if this is part of pipeline and there was pending audio)
+		if [[ $pipeline = "Y" && -d pending_audio && ! -z "$(ls -A pending_audio)" ]]; then
+			# pending_audio folder will contain all necessary info
+			cd pending_audio
 
-	# now add new info about this patient to email alert body (if this is part of pipeline and there was pending audio)
-	if [[ $pipeline = "Y" && -d pending_audio && ! -z "$(ls -A pending_audio)" ]]; then
-		# pending_audio folder will contain all necessary info
-		cd pending_audio
+			# first list transcripts that were successfully pulled
+			# get total number pulled this round to put into header
+			num_pulled=$(find . -maxdepth 1 -name "done+*" -printf '.' | wc -m)
+			echo "" >> "$repo_root"/transcript_lab_email_body.txt 
+			echo "Participant ${p} Open Interview Transcripts Newly Completed/Processed (${num_pulled} Total) - " >> "$repo_root"/transcript_lab_email_body.txt 
+			# loop through files that have been pulled to append the names to email list
+			shopt -s nullglob # if there are no "done" files, this will keep it from running empty loop
+			for file in done+*; do 
+				orig_name=$(echo "$file" | awk -F '+' '{print $2}') # splitting on + now
+				echo "$orig_name" >> "$repo_root"/transcript_lab_email_body.txt
+				mv "$file" ../completed_audio/"orig_name"
+				# also move corresponding transcript to the box push folder!
+				root_name=$(echo "$orig_name" | awk -F '.' '{print $1}')
+				cp ../transcripts/"$root_name".txt "$data_root"/PROTECTED/box_transfer/transcripts/"$root_name".txt
+			done
 
-		# first list transcripts that were successfully pulled
-		# get total number pulled this round to put into header
-		num_pulled=$(find . -maxdepth 1 -name "done+*" -printf '.' | wc -m)
-		echo "" >> "$repo_root"/transcript_lab_email_body.txt 
-		echo "Participant ${p} Open Interview Transcripts Newly Completed/Processed (${num_pulled} Total) - " >> "$repo_root"/transcript_lab_email_body.txt 
-		# loop through files that have been pulled to append the names to email list
-		shopt -s nullglob # if there are no "done" files, this will keep it from running empty loop
-		for file in done+*; do 
-			orig_name=$(echo "$file" | awk -F '+' '{print $2}') # splitting on + now
-			echo "$orig_name" >> "$repo_root"/transcript_lab_email_body.txt
-			mv "$file" ../completed_audio/"orig_name"
-		done
+			# then list transcripts that remain pending
+			# get remaining number of audios to put into header
+			num_pending=$(ls -1 | wc -l)
+			echo "" >> "$repo_root"/transcript_lab_email_body.txt 
+			echo "Participant ${p} Open Interview Transcripts Still Pending (${num_pending} Total) - " >> "$repo_root"/transcript_lab_email_body.txt 
+			# loop through remaining files to append the names to email list
+			shopt -s nullglob # if there are no remaining files, this will keep it from running empty loop
+			for file in *; do 
+				echo "$file" >> "$repo_root"/transcript_lab_email_body.txt
+			done
 
-		# then list transcripts that remain pending
-		# get remaining number of audios to put into header
-		num_pending=$(ls -1 | wc -l)
-		echo "" >> "$repo_root"/transcript_lab_email_body.txt 
-		echo "Participant ${p} Open Interview Transcripts Still Pending (${num_pending} Total) - " >> "$repo_root"/transcript_lab_email_body.txt 
-		# loop through remaining files to append the names to email list
-		shopt -s nullglob # if there are no remaining files, this will keep it from running empty loop
-		for file in *; do 
-			echo "$file" >> "$repo_root"/transcript_lab_email_body.txt
-		done
-
+			cd ..
+		fi
 		cd ..
 	fi
 
 	# same process for psychs
-	cd ../psychs
-	python "$func_root"/interview_transcribeme_sftp_pull.py "psychs" "$data_root" "$study" "$p" "$transcribeme_username" "$transcribeme_password" "$pipeline" "$repo_root"/transcript_lab_email_body.txt
+	if [[ -d psychs ]]; then 
+		cd psychs
+		python "$func_root"/interview_transcribeme_sftp_pull.py "psychs" "$data_root" "$study" "$p" "$transcribeme_username" "$transcribeme_password" "$pipeline" "$repo_root"/transcript_lab_email_body.txt
 
-	
-	if [[ $pipeline = "Y" && -d pending_audio && ! -z "$(ls -A pending_audio)" ]]; then
-		# pending_audio folder will contain all necessary info
-		cd pending_audio
+		
+		if [[ $pipeline = "Y" && -d pending_audio && ! -z "$(ls -A pending_audio)" ]]; then
+			# pending_audio folder will contain all necessary info
+			cd pending_audio
 
-		# first list transcripts that were successfully pulled
-		# get total number pulled this round to put into header
-		num_pulled=$(find . -maxdepth 1 -name "done+*" -printf '.' | wc -m)
-		echo "" >> "$repo_root"/transcript_lab_email_body.txt 
-		echo "Participant ${p} Psychs Interview Transcripts Newly Completed/Processed (${num_pulled} Total) - " >> "$repo_root"/transcript_lab_email_body.txt 
-		# loop through files that have been pulled to append the names to email list
-		shopt -s nullglob # if there are no "done" files, this will keep it from running empty loop
-		for file in done+*; do 
-			orig_name=$(echo "$file" | awk -F '+' '{print $2}') # splitting on + now
-			echo "$orig_name" >> "$repo_root"/transcript_lab_email_body.txt
-			mv "$file" ../completed_audio/"orig_name"
-		done
+			# first list transcripts that were successfully pulled
+			# get total number pulled this round to put into header
+			num_pulled=$(find . -maxdepth 1 -name "done+*" -printf '.' | wc -m)
+			echo "" >> "$repo_root"/transcript_lab_email_body.txt 
+			echo "Participant ${p} Psychs Interview Transcripts Newly Completed/Processed (${num_pulled} Total) - " >> "$repo_root"/transcript_lab_email_body.txt 
+			# loop through files that have been pulled to append the names to email list
+			shopt -s nullglob # if there are no "done" files, this will keep it from running empty loop
+			for file in done+*; do 
+				orig_name=$(echo "$file" | awk -F '+' '{print $2}') # splitting on + now
+				echo "$orig_name" >> "$repo_root"/transcript_lab_email_body.txt
+				mv "$file" ../completed_audio/"orig_name"
+				# also move corresponding transcript to the box push folder!
+				root_name=$(echo "$orig_name" | awk -F '.' '{print $1}')
+				cp ../transcripts/"$root_name".txt "$data_root"/PROTECTED/box_transfer/transcripts/"$root_name".txt
+			done
 
-		# then list transcripts that remain pending
-		# get remaining number of audios to put into header
-		num_pending=$(ls -1 | wc -l)
-		echo "" >> "$repo_root"/transcript_lab_email_body.txt 
-		echo "Participant ${p} Psychs Interview Transcripts Still Pending (${num_pending} Total) - " >> "$repo_root"/transcript_lab_email_body.txt 
-		# loop through remaining files to append the names to email list
-		shopt -s nullglob # if there are no remaining files, this will keep it from running empty loop
-		for file in *; do 
-			echo "$file" >> "$repo_root"/transcript_lab_email_body.txt
-		done
+			# then list transcripts that remain pending
+			# get remaining number of audios to put into header
+			num_pending=$(ls -1 | wc -l)
+			echo "" >> "$repo_root"/transcript_lab_email_body.txt 
+			echo "Participant ${p} Psychs Interview Transcripts Still Pending (${num_pending} Total) - " >> "$repo_root"/transcript_lab_email_body.txt 
+			# loop through remaining files to append the names to email list
+			shopt -s nullglob # if there are no remaining files, this will keep it from running empty loop
+			for file in *; do 
+				echo "$file" >> "$repo_root"/transcript_lab_email_body.txt
+			done
+		fi
 	fi
 
 	# back out of pt folder when done
