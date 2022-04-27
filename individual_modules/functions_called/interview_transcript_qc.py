395c0af1ad0215d7779e4b40ba6285c83efafd20
ennisthemennis <mennis@mit.edu>
2022-04-27 12:24:14 -0400

    change prefix on DPDash CSVs

--- individual_modules/functions_called/interview_transcript_qc.py
+++ individual_modules/functions_called/interview_transcript_qc.py
@@ -211,8 +211,8 @@ def interview_transcript_qc(interview_type, data_root, study, ptID):
 
 	# now prepare to save new CSV for this patient
 	os.chdir(os.path.join(data_root, "GENERAL", study, "processed", ptID, "interviews", interview_type))
-	# Tashrif decided to change the convention for U24 DPDash, so study name in the DPDash CSV name needs to always just be last 2 digits of the site ID here
-	output_path_format = study[-2:]+"-"+ptID+"-interviewRedactedTranscriptQC_" + interview_type + "-day*.csv"
+	# Tashrif decided to change the convention for U24 DPDash, so study name in the DPDash CSV name needs to always just be avlqc identifier here
+	output_path_format = "avlqc"+"-"+ptID+"-interviewRedactedTranscriptQC_" + interview_type + "-day*.csv"
 	output_paths = glob.glob(output_path_format)
 	# delete any old DPDash transcript CSVs
 	cur_day_string = str(study_days[0]) + "to" + str(study_days[-1]) + '.csv' # check to see if the new one is actually new though
@@ -221,8 +221,8 @@ def interview_transcript_qc(interview_type, data_root, study, ptID):
 			return # do nothing if we already have!
 		os.remove(old_dp)
 	# save the current one finally
-	# study name in the DPDash CSV name needs to always just be last 2 digits of the site ID here
-	output_path_cur = study[-2:] + "-" + ptID + "-interviewRedactedTranscriptQC_" + interview_type + "-day" + str(study_days[0]) + "to" + str(study_days[-1]) + '.csv'
+	# study name in the DPDash CSV name needs to always just be avlqc identifier here
+	output_path_cur = "avlqc" + "-" + ptID + "-interviewRedactedTranscriptQC_" + interview_type + "-day" + str(study_days[0]) + "to" + str(study_days[-1]) + '.csv'
 	new_csv.to_csv(output_path_cur,index=False)
 	return
 			
