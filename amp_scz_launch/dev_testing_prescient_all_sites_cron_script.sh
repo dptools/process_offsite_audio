c10e7ea082caf7b1f050b97e9dbce62477bc5ffb
ennisthemennis <mennis@mit.edu>
2022-05-29 12:56:18 -0400

    initial prescient configuration

---
+++ amp_scz_launch/dev_testing_prescient_all_sites_cron_script.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# need to ensure that my account can write to processed folders, because Lochness does not always create the interview subfolders for a subject ID (needs to be one in GENERAL and PROTECTED)
+# this is just a workaround for the dev server right now, for production we need to figure out an actual structure for file permissions
+sudo chmod -R 770 /mnt/prescient/Prescient_data_sync/PHOENIX/*/*/processed
+
+# activate python environment
+. /home/cho/miniconda3/etc/profile.d/conda.sh
+conda activate audio_process
+
+# run audio side code for each config
+for file in /home/cho/soft/process_offsite_audio/amp_scz_launch/dev_testing_pronet_site_configs/*.sh; do
+	bash /home/cho/soft/process_offsite_audio/interview_audio_process.sh "$file"
+	# add spacing for when monitoring logs in real time
+	echo ""
+	echo ""
+done
+
+# run transcript side code for each config
+for file in /home/cho/soft/process_offsite_audio/amp_scz_launch/dev_testing_pronet_site_configs/*.sh; do
+	bash /home/cho/soft/process_offsite_audio/interview_transcript_process.sh "$file"
+	# add spacing for when monitoring logs in real time
+	echo ""
+	echo ""
+done
+
+# run video code for each config
+for file in /home/cho/soft/process_offsite_audio/amp_scz_launch/dev_testing_pronet_site_configs/*.sh; do
+	bash /home/cho/soft/process_offsite_audio/interview_video_process.sh "$file"
+	# add spacing for when monitoring logs in real time
+	echo ""
+	echo ""
+done
+
+# run accounting summary compilation for each config
+for file in /home/cho/soft/process_offsite_audio/amp_scz_launch/dev_testing_pronet_site_configs/*.sh; do
+	bash /home/cho/soft/process_offsite_audio/interview_summary_checks.sh "$file"
+	# add spacing for when monitoring logs in real time
+	echo ""
+	echo ""
+done
+
+# at the end of the processing, make sure that all processed interview outputs are in the pronet group, so they can be appropriately synced back to datalake
+# this is also a temporary workaround for dev server
+sudo chgrp -R prescient /mnt/prescient/Prescient_data_sync/PHOENIX/*/*/processed/*
+# similarly make sure for the box transfer folder!
+sudo chgrp -R prescient /mnt/prescient/Prescient_data_sync/PHOENIX/PROTECTED/box_transfer
+
+# finally run the utility for stats combined across sites
+bash /home/cho/soft/process_offsite_audio/amp_scz_launch/final_all_sites_utility.sh /mnt/prescient/Prescient_data_sync/PHOENIX mennis2@partners.org PrescientDev
---
+++ amp_scz_launch/mediaflux_push_utility.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# call this with data root argument, so it can be run from dev and prod independently for prescient
+data_root="$1"
+
+# get absolute path to dir script is being run from, to save history of emails in logs and find relevant functions
+full_path=$(realpath $0)
+launch_root=$(dirname $full_path)
+repo_root="$launch_root"/..
+func_root="$launch_root"/helper_functions
+
+mediaflux_username="michaela.ennis"
+passwords_path="$repo_root"/.passwords.sh
+source "$passwords_path"
+
+# make directory for logs if needed
+if [[ ! -d ${repo_root}/logs ]]; then
+	mkdir "$repo_root"/logs
+fi
+# keep logs from this in a mediaflux folder
+if [[ ! -d ${repo_root}/logs/MediaFlux ]]; then
+	mkdir "$repo_root"/logs/MediaFlux
+fi
+# organize log filenames via date
+cur_date=$(date +%Y%m%d)
+exec >  >(tee -ia "$repo_root"/logs/MediaFlux/"$cur_date".txt)
+exec 2> >(tee -ia "$repo_root"/logs/MediaFlux/"$cur_date".txt >&2)
+
+# TODO: 
+# echo to logs
+# run python functions from func_root to do the actual mediaflux push, using dataroot, username, and password information (mediaflux_password)
+
+# TODO: make sure this gets called appropriately from the prescient cron script(s)! will be once at the end and handle across sites that way
+
