96fdfd373f904c4048030e85da50b6187e5e95d9
ennisthemennis <mennis@mit.edu>
2023-01-18 16:54:47 -0500

    prescient emails bug fix

--- amp_scz_launch/helper_functions/tables_summary.py
+++ amp_scz_launch/helper_functions/tables_summary.py
@@ -18,28 +18,53 @@ def run_summary_operation_tables(input_csv,output_folder,cur_server):
 
 	cur_date = date.today().strftime("%m/%d/%Y")
 
-	final_table_counts = input_df[["study","interview_type","patient","overall_db","mean_face_area","total_words"]].groupby([pd.Categorical(input_df.study),"interview_type"]).count().fillna(0).astype(int)
-	final_table_counts.rename(columns={"patient":"processed_interviews_count-any_modality","overall_db":"processed_audio_count","mean_face_area":"processed_video_count","total_words":"processed_transcript_count"},inplace=True)
-	
-	inaud_filt = input_df[input_df["inaudible_per_word"]<0.01]
-	good_qual_count = inaud_filt[["study", "interview_type", "inaudible_per_word"]].groupby([pd.Categorical(inaud_filt.study),"interview_type"]).count().rename(columns={"inaudible_per_word":"good_transcript_quality_count"})
+	# avoid unhashable type error on Prescient
+	try:
+		final_table_counts = input_df[["study","interview_type","patient","overall_db","mean_face_area","total_words"]].groupby([pd.Categorical(input_df.study),"interview_type"]).count().fillna(0).astype(int)
+		final_table_counts.rename(columns={"patient":"processed_interviews_count-any_modality","overall_db":"processed_audio_count","mean_face_area":"processed_video_count","total_words":"processed_transcript_count"},inplace=True)
+		
+		inaud_filt = input_df[input_df["inaudible_per_word"]<0.01]
+		good_qual_count = inaud_filt[["study", "interview_type", "inaudible_per_word"]].groupby([pd.Categorical(inaud_filt.study),"interview_type"]).count().rename(columns={"inaudible_per_word":"good_transcript_quality_count"})
 
-	inaud_filt_partial = input_df[(input_df["inaudible_per_word"]>=0.01)&(input_df["inaudible_per_word"]<0.05)]
-	partial_qual_count = inaud_filt_partial[["study", "interview_type", "inaudible_per_word"]].groupby([pd.Categorical(inaud_filt_partial.study),"interview_type"]).count().rename(columns={"inaudible_per_word":"partial_transcript_quality_count"})
+		inaud_filt_partial = input_df[(input_df["inaudible_per_word"]>=0.01)&(input_df["inaudible_per_word"]<0.05)]
+		partial_qual_count = inaud_filt_partial[["study", "interview_type", "inaudible_per_word"]].groupby([pd.Categorical(inaud_filt_partial.study),"interview_type"]).count().rename(columns={"inaudible_per_word":"partial_transcript_quality_count"})
 
-	faces_filt = input_df[(input_df["mean_faces_detected_in_frame"]>1.8) & (input_df["mean_faces_detected_in_frame"]<2.3)]
-	good_face_count = faces_filt[["study", "interview_type", "mean_faces_detected_in_frame"]].groupby([pd.Categorical(faces_filt.study),"interview_type"]).count().rename(columns={"mean_faces_detected_in_frame":"good_video_quality_count"})
+		faces_filt = input_df[(input_df["mean_faces_detected_in_frame"]>1.8) & (input_df["mean_faces_detected_in_frame"]<2.3)]
+		good_face_count = faces_filt[["study", "interview_type", "mean_faces_detected_in_frame"]].groupby([pd.Categorical(faces_filt.study),"interview_type"]).count().rename(columns={"mean_faces_detected_in_frame":"good_video_quality_count"})
 
-	faces_filt_partial = input_df[((input_df["mean_faces_detected_in_frame"]>1.1) & (input_df["mean_faces_detected_in_frame"]<=1.8)) | (input_df["mean_faces_detected_in_frame"]>=2.3)]
-	partial_face_count = faces_filt_partial[["study", "interview_type", "mean_faces_detected_in_frame"]].groupby([pd.Categorical(faces_filt_partial.study),"interview_type"]).count().rename(columns={"mean_faces_detected_in_frame":"partial_video_quality_count"})
+		faces_filt_partial = input_df[((input_df["mean_faces_detected_in_frame"]>1.1) & (input_df["mean_faces_detected_in_frame"]<=1.8)) | (input_df["mean_faces_detected_in_frame"]>=2.3)]
+		partial_face_count = faces_filt_partial[["study", "interview_type", "mean_faces_detected_in_frame"]].groupby([pd.Categorical(faces_filt_partial.study),"interview_type"]).count().rename(columns={"mean_faces_detected_in_frame":"partial_video_quality_count"})
 
-	all_counts = final_table_counts.merge(good_qual_count, how="outer", left_index=True, right_index=True).merge(good_face_count, how="outer", left_index=True, right_index=True).merge(partial_qual_count, how="outer", left_index=True, right_index=True).merge(partial_face_count, how="outer", left_index=True, right_index=True)
-	formatted_table = all_counts[["processed_interviews_count-any_modality","processed_video_count","good_video_quality_count","partial_video_quality_count","processed_audio_count","processed_transcript_count","good_transcript_quality_count","partial_transcript_quality_count"]].fillna(0).astype(int)
+		all_counts = final_table_counts.merge(good_qual_count, how="outer", left_index=True, right_index=True).merge(good_face_count, how="outer", left_index=True, right_index=True).merge(partial_qual_count, how="outer", left_index=True, right_index=True).merge(partial_face_count, how="outer", left_index=True, right_index=True)
+		formatted_table = all_counts[["processed_interviews_count-any_modality","processed_video_count","good_video_quality_count","partial_video_quality_count","processed_audio_count","processed_transcript_count","good_transcript_quality_count","partial_transcript_quality_count"]].fillna(0).astype(int)
 
-	mean_lengths = input_df[["study","interview_type","length_minutes"]].groupby([pd.Categorical(input_df.study),"interview_type"]).mean()
-	mean_lengths["mean_interview_audio_minutes"] = [round(x,2) for x in mean_lengths["length_minutes"].tolist()]
-	mean_lengths.drop(columns=["length_minutes"],inplace=True)
-	table_addendum = formatted_table.merge(mean_lengths, how="outer", left_index=True, right_index=True)
+		mean_lengths = input_df[["study","interview_type","length_minutes"]].groupby([pd.Categorical(input_df.study),"interview_type"]).mean()
+		mean_lengths["mean_interview_audio_minutes"] = [round(x,2) for x in mean_lengths["length_minutes"].tolist()]
+		mean_lengths.drop(columns=["length_minutes"],inplace=True)
+		table_addendum = formatted_table.merge(mean_lengths, how="outer", left_index=True, right_index=True)
+	except:
+		final_table_counts = input_df[["study","interview_type","patient","overall_db","mean_face_area","total_words"]].groupby(["study","interview_type"]).count().fillna(0).astype(int)
+		final_table_counts.rename(columns={"patient":"processed_interviews_count-any_modality","overall_db":"processed_audio_count","mean_face_area":"processed_video_count","total_words":"processed_transcript_count"},inplace=True)
+		
+		inaud_filt = input_df[input_df["inaudible_per_word"]<0.01]
+		good_qual_count = inaud_filt[["study", "interview_type", "inaudible_per_word"]].groupby(["study","interview_type"]).count().rename(columns={"inaudible_per_word":"good_transcript_quality_count"})
+
+		inaud_filt_partial = input_df[(input_df["inaudible_per_word"]>=0.01)&(input_df["inaudible_per_word"]<0.05)]
+		partial_qual_count = inaud_filt_partial[["study", "interview_type", "inaudible_per_word"]].groupby(["study","interview_type"]).count().rename(columns={"inaudible_per_word":"partial_transcript_quality_count"})
+
+		faces_filt = input_df[(input_df["mean_faces_detected_in_frame"]>1.8) & (input_df["mean_faces_detected_in_frame"]<2.3)]
+		good_face_count = faces_filt[["study", "interview_type", "mean_faces_detected_in_frame"]].groupby(["study","interview_type"]).count().rename(columns={"mean_faces_detected_in_frame":"good_video_quality_count"})
+
+		faces_filt_partial = input_df[((input_df["mean_faces_detected_in_frame"]>1.1) & (input_df["mean_faces_detected_in_frame"]<=1.8)) | (input_df["mean_faces_detected_in_frame"]>=2.3)]
+		partial_face_count = faces_filt_partial[["study", "interview_type", "mean_faces_detected_in_frame"]].groupby(["study","interview_type"]).count().rename(columns={"mean_faces_detected_in_frame":"partial_video_quality_count"})
+
+		all_counts = final_table_counts.merge(good_qual_count, how="outer", left_index=True, right_index=True).merge(good_face_count, how="outer", left_index=True, right_index=True).merge(partial_qual_count, how="outer", left_index=True, right_index=True).merge(partial_face_count, how="outer", left_index=True, right_index=True)
+		formatted_table = all_counts[["processed_interviews_count-any_modality","processed_video_count","good_video_quality_count","partial_video_quality_count","processed_audio_count","processed_transcript_count","good_transcript_quality_count","partial_transcript_quality_count"]].fillna(0).astype(int)
+
+		mean_lengths = input_df[["study","interview_type","length_minutes"]].groupby(["study","interview_type"]).mean()
+		mean_lengths["mean_interview_audio_minutes"] = [round(x,2) for x in mean_lengths["length_minutes"].tolist()]
+		mean_lengths.drop(columns=["length_minutes"],inplace=True)
+		table_addendum = formatted_table.merge(mean_lengths, how="outer", left_index=True, right_index=True)
 
 	abbrev_table = table_addendum[["processed_interviews_count-any_modality","good_video_quality_count","partial_video_quality_count","good_transcript_quality_count","partial_transcript_quality_count","mean_interview_audio_minutes"]]
 	abbrev_table = abbrev_table.rename(columns={"processed_interviews_count-any_modality":"processed_interviews_count"})
