9bfe15e9b283bb81257518035758411622d22513
Dheshan Mohandass <dmohandass@mgh.harvard.edu>
2024-05-08 03:23:36 +1000

    feat.: Migrate to new environment (PRESCIENT)
    Signed-off-by: Dheshan Mohandass <dmohandass@mgh.harvard.edu>

--- amp_scz_launch/production_prescient_site_configs/PrescientCG.sh
+++ amp_scz_launch/production_prescient_site_configs/PrescientCG.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # basic path settings
-data_root="/mnt/prescient/Prescient_production/PHOENIX"
+data_root="/var/lib/prescient/data/PHOENIX"
 study="PrescientCG"
 export data_root
 export study
